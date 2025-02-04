25f3e22ea3218edb6633fac9fba8d13f04e5bde4
owenodaly <spdpood@kcl.ac.uk>
2025-02-04 20:59:45 +0000

    Update timing.py

--- timing.py
+++ timing.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 import scipy
-st.info(f"SciPy version: {scipy.__version__}")
-res = stats.goodness_of_fit(stats.norm, pseudo_data, known_params=known_params,
-                                    statistic='ks', random_state=rng)
+#st.info(f"SciPy version: {scipy.__version__}")
+#res = stats.goodness_of_fit(stats.norm, pseudo_data, known_params=known_params,
+#                                    statistic='ks', random_state=rng)
 from scipy.interpolate import interp1d
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
